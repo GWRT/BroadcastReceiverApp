8feb4212f4a45d56907a2af0c2d5db9d5505b0e5
GWRT <gramosti@unsa.edu.pe>
2022-10-31 18:34:56 -0500

    Initial commit

---
+++ app/src/main/java/com/idnp/broadcastreceiverapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.idnp.broadcastreceiverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        IntentFilter intentFilter = new IntentFilter("com.idnp.lab04");
+        MyBroadcastReceiver objReceiver = new MyBroadcastReceiver();
+        registerReceiver(objReceiver, intentFilter);
+    }
+}
---
+++ app/src/main/java/com/idnp/broadcastreceiverapp/MyBroadcastReceiver.java
@@ -0,0 +1,21 @@
+package com.idnp.broadcastreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+        String msg = intent.getExtras().getString("msg");
+
+        Log.i("BroadcastReceiver", "Broadcast message is received");
+        Toast.makeText(context, "Broadcast message is received: "+ msg, Toast.LENGTH_LONG).show();
+    }
+}
